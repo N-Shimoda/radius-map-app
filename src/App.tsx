04ad071ed1ef3b95652094d6fe9320f6dc2a1ab4
N-Shimoda <naoki.shimoda@outlook.jp>
2025-12-17 00:42:18 +0900

    updated the sidebar icon

--- src/App.tsx
+++ src/App.tsx
@@ -241,18 +241,9 @@ export default function App() {
               strokeLinejoin="round"
               aria-hidden="true"
             >
-              {isSidebarOpen ? (
-                <>
-                  <line x1="4" y1="12" x2="20" y2="12" />
-                  <polyline points="10 18 4 12 10 6" />
-                </>
-              ) : (
-                <>
-                  <line x1="4" y1="6" x2="20" y2="6" />
-                  <line x1="4" y1="12" x2="20" y2="12" />
-                  <line x1="4" y1="18" x2="20" y2="18" />
-                </>
-              )}
+              <line x1="4" y1="6" x2="20" y2="6" />
+              <line x1="4" y1="12" x2="20" y2="12" />
+              <line x1="4" y1="18" x2="20" y2="18" />
             </svg>
           </button>
           <div>
